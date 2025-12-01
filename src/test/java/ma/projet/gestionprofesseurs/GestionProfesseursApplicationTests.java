4818496d53f79d5a10a6c573f8715decc821b73e
Mubashir_Mehran <mubashir.dev01@gmail.com>
2025-12-01 22:21:04 +0100

    Fix test: Remove @Profile from TestDatabaseConfig and exclude DataSourceAutoConfiguration to prevent MySQL connection

--- src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
+++ src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
@@ -1,13 +1,12 @@
 package ma.projet.gestionprofesseurs;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(excludeAutoConfiguration = DataSourceAutoConfiguration.class)
 @Import(TestDatabaseConfig.class)
 @TestPropertySource(properties = {
     "spring.jpa.hibernate.ddl-auto=create-drop",
--- src/test/java/ma/projet/gestionprofesseurs/TestDatabaseConfig.java
+++ src/test/java/ma/projet/gestionprofesseurs/TestDatabaseConfig.java
@@ -4,12 +4,10 @@ import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @TestConfiguration
-@Profile("test")
 public class TestDatabaseConfig {
 
     @Bean

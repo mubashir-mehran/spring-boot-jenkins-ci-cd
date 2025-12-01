f43b4906a4e77152ad00c4ac7469ab6805a793cf
Mubashir_Mehran <mubashir.dev01@gmail.com>
2025-12-01 22:02:40 +0100

    Fix test: Create test-specific application.properties in src/test/resources to use H2

--- src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
+++ src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
@@ -1,21 +1,9 @@
 package ma.projet.gestionprofesseurs;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=",
-    "spring.jpa.hibernate.ddl-auto=create-drop",
-    "spring.jpa.show-sql=false",
-    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
-})
 class GestionProfesseursApplicationTests {
 
     @Test

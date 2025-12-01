e238ed41ba13d8bac6adccc3cb805360bd8c30d9
Mubashir_Mehran <mubashir.dev01@gmail.com>
2025-12-01 18:53:39 +0100

    Fix test database configuration: Use @SpringBootTest properties and @AutoConfigureTestDatabase for H2

--- src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
+++ src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
@@ -1,12 +1,11 @@
 package ma.projet.gestionprofesseurs;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
     "spring.datasource.driver-class-name=org.h2.Driver",
     "spring.datasource.username=sa",
     "spring.datasource.password=",
@@ -14,6 +13,7 @@ import org.springframework.test.context.TestPropertySource;
     "spring.jpa.show-sql=false",
     "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
 })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class GestionProfesseursApplicationTests {
 
     @Test

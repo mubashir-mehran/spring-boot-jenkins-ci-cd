4e02b986f48240185ece1da7204dc6707b6e8a3c
Mubashir_Mehran <mubashir.dev01@gmail.com>
2025-12-01 22:07:48 +0100

    Fix test: Exclude DataSourceAutoConfiguration and use @AutoConfigureTestDatabase with explicit H2 properties

--- src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
+++ src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
@@ -1,10 +1,13 @@
 package ma.projet.gestionprofesseurs;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(excludeAutoConfiguration = DataSourceAutoConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @TestPropertySource(properties = {
     "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
     "spring.datasource.driver-class-name=org.h2.Driver",

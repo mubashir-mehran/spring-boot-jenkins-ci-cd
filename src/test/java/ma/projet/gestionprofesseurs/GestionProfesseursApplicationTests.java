506379ed7925b4a2189653310fad0d4680c20153
Mubashir_Mehran <mubashir.dev01@gmail.com>
2025-12-01 18:47:08 +0100

    Fix test profile activation: Use @TestPropertySource to directly configure H2 database

--- src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
+++ src/test/java/ma/projet/gestionprofesseurs/GestionProfesseursApplicationTests.java
@@ -2,10 +2,18 @@ package ma.projet.gestionprofesseurs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.show-sql=false",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class GestionProfesseursApplicationTests {
 
     @Test
